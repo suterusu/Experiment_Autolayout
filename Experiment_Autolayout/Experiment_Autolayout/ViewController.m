f51b3f269e1116ab55582552f4b6013a64e80839
Inba <git@bitbucket.org:Suterusu1988/memorybrowser.git>
2017-12-18 03:34:07 +0900

    init project

---
+++ Experiment_Autolayout/Experiment_Autolayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Experiment_Autolayout
+//
+//  Created by Inba on 2017/12/18.
+//  Copyright © 2017年 Inba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
